ffbcec380f664c096773e6a2fbb410a0704a1a85
giselaparmelo <giselaparmelo@gmail.com>
2025-01-04 18:53:40 -0300

    primer grabacion

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ItemListContainer from './components/ItemListContainer'
+import NavBar from './components/navBar'
+
+function App() {
+ 
+  return (
+    <>
+      <NavBar />
+      <ItemListContainer />
+    </>
+  )
+}
+
+export default App
